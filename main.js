4a4fccd7d5fbd913925e122d657305f42c335d1d
Jay Doerga <jay@jjdeveloping.nl>
2021-03-31 09:31:00 +0200

    New env

--- main.js
+++ main.js
@@ -1,23 +1,14 @@
 const Twitter = require('twitter')
+require('dotenv').config()
 const client = new Twitter({
-    consumer_key: 'yynNvCthh8luOYBwJQxCCnTCQ',
-    consumer_secret: 'sKIcWHZQscY2bYCAGPKWT3IrnzZc7oNFUEhspErcYJDWVqTv8N',
-    // bearer_token: 'AAAAAAAAAAAAAAAAAAAAAEuXOAEAAAAAbpwV9ZoWZ0ajiMwSG6unuvkjfe0%3DZoRcu5UyQC2pLxgabagsSm9AQzF0gRLFmIkHvVtls7BGkRS9Cg',
-    access_token_key: '1033768931096453121-yhnRZUngJYHPP0LOwO0vTXQoQDJaFI',
-    access_token_secret: 'xxBHUqSdJPNEdTKWW0SZQslZhDFeNZ5e5wWirapD2PZLN'
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token_key: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET
 });
 const Quote = require('inspirational-quotes');
 let isRandom = false;
 let body = "Twitter for android/iOS? Lmao, ik gebruik goat shit"
-
-
-
-
-// client.post('statuses/update', { status: body }, function (error, tweet, response) {
-//     if (error) throw error;
-//     console.log(tweet);  // Tweet body.
-//     //console.log(response);  // Raw response object.
-// });
 
 const hours = 12;
 
